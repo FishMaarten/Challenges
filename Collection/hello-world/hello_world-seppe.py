776a27c4c2dcf3a67122e7c23aae329928dba773
SeppeSeghers <seppeseghers1@gmail.com>
2020-06-18 16:34:18 +0200

    hello-world ADD seppe

---
+++ Collection/hello-world/hello_world-seppe.py
@@ -0,0 +1,7 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+# takes a string <name>, if no string is given, assumes it as 'World', then returns a string "Hello, <name>!"
+def hello(name='World'):
+    return "Hello, {}!".format(name)
