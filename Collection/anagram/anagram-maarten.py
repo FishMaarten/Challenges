d1d6c7d18e12829abcb7de297a5091129bc72f69
FishMaarten <fishmaarten@gmail.com>
2020-06-20 12:43:46 +0200

    anagram EDIT maarten

--- Collection/anagram/anagram-maarten.py
+++ Collection/anagram/anagram-maarten.py
@@ -1,9 +1,8 @@
 def detect_anagrams(word:str, candidates:[]) -> []: 
-    """ Checks if char_count from candidates matches that of the given word """
-    return [w for w in candidates if w.lower() != word.lower() and character_count(w.lower()) == character_count(word.lower())]
+    """ Checks if character_count from candidates matches that of the given word """
+    return [w for w in candidates if w.lower() != word.lower()
+            and character_count(w.lower()) == character_count(word.lower())]
 
 def character_count(word: str) -> {}:
     """ Returns a dictionary with the count of each unique character in word """
-    return {c:word.count(c) for c in word}
-
-print(detect_anagrams("banana", ["sgdfs", "abnana", "banana"]))
+    return {c:word.lower().count(c) for c in word.lower()}
